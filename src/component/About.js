9e2b5c72099904e2a27656dc8cd2ecedab9711b3
MomoSaidu <145624886+MomoSaidu@users.noreply.github.com>
2023-11-17 14:49:54 +0000

    Third commit

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about-container">
+      <h2>About Us</h2>
+      <p>
+        Welcome to our website! We are RecycleNow. Our mission is dedicated to developing and delivering high quality, cost effective solid waste reduction, recycling, and refuse programs that provide and promote sustainability in our communities.
+      </p>
+      <p>
+        At RecycleNow, we believe in the betterment of our future on this planet.
+        Our team is dedicated to [your team's dedication or expertise].
+      </p>
+      <h3>Our Team</h3>
+      <p>
+        Meet the talented individuals who make up our team and contribute to the
+        success of [Your Company/Project Name].
+      </p>
+      {/* You can add team member profiles or any other relevant information */}
+      <h3>What we do</h3>
+      <p>
+        RecycleNow provides solid waste services for irish residents and businesses.  RecycleNow has contracted with Republic Services for the collection, transfer and disposal of residential and commercial garbage, recycling and organics for the processing of residential and commercial recyclable materials.
+      </p>
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions or would like to get in touch with us, feel
+        free to contact us at RecycleNow@....
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,9 +5,16 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className="header">
-      <h1 className="title">RecycleNow</h1>
+      <h1 className="title">
+        <Link to="/">
+          <img
+            src="Untitled.jpeg" // Update the path to your image
+            alt="RecycleNow Logo"
+            style={{ width: '200px', height: 'auto' }}
+          />
+        </Link>
+      </h1>
       <nav className="navbar">
-        <Link to="/">Home</Link>
         <Link to="/recycle-item">Recycle Item</Link>
         <Link to="/where-to-recycle">Where To Recycle</Link>
         <Link to="/how-to-recycle">How To Recycle</Link>
